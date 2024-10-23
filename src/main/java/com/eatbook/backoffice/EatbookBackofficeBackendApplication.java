3e0e2f1837ef3f1f294a85b91f9bebbaa4510a1f
KIM TAE HYEONG <thk12378905@gmail.com>
2024-10-23 11:39:29 +0900

    chore: 프로젝트 초기 구조 설정

---
+++ src/main/java/com/eatbook/backoffice/EatbookBackofficeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eatbook.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatbookBackofficeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatbookBackofficeBackendApplication.class, args);
+	}
+
+}
