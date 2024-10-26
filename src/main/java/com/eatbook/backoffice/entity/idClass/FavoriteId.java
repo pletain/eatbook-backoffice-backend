2e51578ea14c186dbdcd81df9b06c60f0f9f6096
KIM TAE HYEONG <thk12378905@gmail.com>
2024-10-26 13:52:49 +0900

    feat: BookmarkId, FavoriteId idClass 생성

---
+++ src/main/java/com/eatbook/backoffice/entity/idClass/FavoriteId.java
@@ -0,0 +1,18 @@
+package com.eatbook.backoffice.entity.idClass;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class FavoriteId implements Serializable {
+    private static final long serialVersionUID = 4L;
+
+    private String novelId;
+    private String memberId;
+
+}
