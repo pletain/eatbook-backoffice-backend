50ca7c5069af1b0c3b5530ae369370f5b73a1b8e
KIM TAE HYEONG <thk12378905@gmail.com>
2024-10-26 14:43:58 +0900

    feat: NovelAuthor 클래스 생성

---
+++ src/main/java/com/eatbook/backoffice/entity/NovelAuthor.java
@@ -0,0 +1,54 @@
+package com.eatbook.backoffice.entity;
+
+import com.eatbook.backoffice.entity.idClass.NovelAuthorId;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@IdClass(NovelAuthorId.class)
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "novel_author")
+public class NovelAuthor {
+
+    @Id
+    @Column(name = "novel_id", length = 36)
+    @NotNull
+    private String novelId;
+
+    @Id
+    @Column(name = "author_id", length = 36)
+    @NotNull
+    private String authorId;
+
+    @Column(nullable = false)
+    @NotNull
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    @NotNull
+    private Novel novel;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    @NotNull
+    private Author author;
+
+    @Builder
+    public NovelAuthor(String novelId, String authorId, LocalDateTime createdAt, Novel novel, Author author) {
+        this.novelId = novelId;
+        this.authorId = authorId;
+        this.createdAt = createdAt;
+        this.novel = novel;
+        this.author = author;
+    }
+
+}
