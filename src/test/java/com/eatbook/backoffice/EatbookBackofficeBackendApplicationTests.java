a131a9dc91f3b7b14bb11f880cf76f345502638b
KIM TAE HYEONG <thk12378905@gmail.com>
2024-10-29 13:01:44 +0900

    [build/EATBOOK-58] 스켈레톤 코드 작성 및 Git Submodule 추가 (#1)
    * chore: 프로젝트 초기 구조 설정
    
    * chore: 마이그레이션 파일 추가: 데이터베이스 및 초기 테이블 생성
    
    * chore: 도메인별 패키지 구조 구성
    
    * chore: DDL 스크립트 수정
    search_log과 사용자 테이블 간의 관계를 제거 (user_id FK)
    search_log의 일부 필드 삭제 (updated_at, deleted_at) => soft-delete 될 일이 없고 계속 기록이 쌓이는 구조로 생각
    
    * chore: 도메인 패키지에서 entity 패키지 제거
    
    * chore: entity 패키지 추가
    
    * chore: global 패키지 추가
    
    * build: 설정 및 마이그레이션 파일 제거
    - 설정 관리를 서브모듈로 통합하기 위해 application.yml 삭제
    
    - 사용되지 않는 데이터베이스 및 초기 테이블 SQL 마이그레이션 파일 제거
    
    * build: 설정 및 마이그레이션 파일 복사 작업 추가
    
    * chore: .gitignore에 모든 .env 파일을 무시하도록 추가
    
    * build: 최신 서브모듈 상태 적용

---
+++ src/test/java/com/eatbook/backoffice/EatbookBackofficeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatbook.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatbookBackofficeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
