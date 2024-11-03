163d879888ec88c3b10e3e51d8c1a432115a06c2
KIM TAE HYEONG <thk12378905@gmail.com>
2024-11-04 07:31:09 +0900

    docs: Novel Controller javadoc 수정

--- src/main/java/com/eatbook/backoffice/domain/novel/controller/NovelController.java
+++ src/main/java/com/eatbook/backoffice/domain/novel/controller/NovelController.java
@@ -2,6 +2,7 @@ package com.eatbook.backoffice.domain.novel.controller;
 
 import com.eatbook.backoffice.domain.novel.dto.NovelRequest;
 import com.eatbook.backoffice.domain.novel.dto.NovelResponse;
+import com.eatbook.backoffice.domain.novel.response.NovelSuccessCode;
 import com.eatbook.backoffice.domain.novel.service.NovelService;
 import com.eatbook.backoffice.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -31,8 +32,10 @@ public class NovelController {
     /**
      * 새로운 소설을 생성합니다.
      *
-     * @param novelRequest 소설 세부 정보가 포함된 요청 객체.
-     * @return HTTP 상태 201 (Created)과 성공 코드와 생성된 소설Id와 커버 이미지용 presigned URL 정보가 포함된 ApiResponse를 포함하는 ResponseEntity.
+     * @param novelRequest 소설 세부 정보가 포함된 요청 객체. 이 객체는 Spring의 {@link Validated} 어노테이션을 사용하여 유효성을 검사해야 합니다.
+     * @return {@link HttpStatus#CREATED} 상태 코드를 갖는 ResponseEntity와
+     *         성공 코드 {@link NovelSuccessCode#NOVEL_CREATED}를 포함하는 ApiResponse입니다.
+     *         이 ApiResponse에는 생성된 소설Id와 커버 이미지용 presigned URL 정보가 포함됩니다.
      */
     @PostMapping
     public ResponseEntity<ApiResponse> createNovel(@ModelAttribute @Validated NovelRequest novelRequest) {
