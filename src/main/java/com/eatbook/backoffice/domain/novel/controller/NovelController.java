9fe4e3e8fa5899c40bf26edce9cd1ea83e538895
KIM TAE HYEONG <thk12378905@gmail.com>
2024-11-03 18:24:53 +0900

    docs: 컨트롤러 설명 추가

--- src/main/java/com/eatbook/backoffice/domain/novel/controller/NovelController.java
+++ src/main/java/com/eatbook/backoffice/domain/novel/controller/NovelController.java
@@ -16,14 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static com.eatbook.backoffice.domain.novel.response.NovelSuccessCode.NOVEL_CREATED;
 
+/**
+ * 소설 관련 작업을 관리하는 컨트롤러.
+ *
+ * @author lavin
+ */
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/admin/novel")
 public class NovelController {
-
     private final NovelService novelService;
 
+    /**
+     * 새로운 소설을 생성합니다.
+     *
+     * @param novelRequest 소설 세부 정보가 포함된 요청 객체.
+     * @return HTTP 상태 201 (Created)과 성공 코드와 생성된 소설Id와 커버 이미지용 presigned URL 정보가 포함된 ApiResponse를 포함하는 ResponseEntity.
+     */
     @PostMapping
     public ResponseEntity<ApiResponse> createNovel(@ModelAttribute @Validated NovelRequest novelRequest) {
 
